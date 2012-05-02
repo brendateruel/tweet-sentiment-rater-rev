04bde680bd28796eeb82019af7ba07178485053d
Brenda <brendateruel@aol.com>
2012-05-02 00:39:08 -0500

    copied project to new repo with updated files to protect passwords

---
+++ sentiment-analysis2.php
@@ -0,0 +1,19 @@
+﻿<?php
+
+require("../secret.php");
+
+include "module/AlchemyAPI_CURL.php";
+include "module/AlchemyAPIParams.php";
+
+$alchemyObj = new AlchemyAPI();
+$alchemyObj->loadAPIKey("../alchemy_api_key.txt");
+
+$a = "kindle%20is%20amazing";
+
+$result = $alchemyObj->TextGetTextSentiment($a);
+echo "$result<br /><br />\n";
+
+
+  
+?>
+
