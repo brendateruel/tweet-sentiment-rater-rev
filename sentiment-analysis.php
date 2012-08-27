0da63c0308180ee2b9cb10236874c91fc56d1a2d
Brenda <brendateruel@aol.com>
2012-08-26 19:13:10 -0500

    testing out retrieval of tweets to run through sentiment analysis but $result is still an array; $text is unable to urlencode because not a string

--- sentiment-analysis.php
+++ sentiment-analysis.php
@@ -1,6 +1,13 @@
 ﻿<?php
 
 require("../secret.php");
+require("twitteroauth/twitteroauth-xml.php");
+session_start();
+
+if(!empty($_SESSION['username'])){  
+    $twitteroauth = new TwitterOAuth($tOauth_apiKey, $tOauth_apiSecret, $_SESSION['oauth_token'], $_SESSION['oauth_secret']);  
+	$session_username = $_SESSION['username'];
+}  
 
   /**
    * GET wrapper for oAuthRequest.
@@ -12,28 +19,58 @@ require("../secret.php");
     }
     return $response;
   }
+ 
 
-$tweet = mysql_query("SELECT tweet, status_ID FROM temp_timeline WHERE user_handle='bergatron'");  
-
+$new_friends_table = "friends_" . $session_username;
+$new_temp_timeline = "temp_timeline_" . $session_username; 
+  
+  
+if(!($stmt = $mysqli->prepare("SELECT tweet, status_ID FROM {$new_temp_timeline} WHERE user_handle='adactio'"))) {
+	     echo "Prepare failed: (" . $mysqli->errno . ") " . $mysqli->error;
+	}
+if (!$stmt->execute()) {
+	     echo "Execution failed: (" . $mysqli->errno . ") " . $mysqli->error;
+	}
+	
+/*
 $text = array();
 $status_id = array();
+$tweet = array();
 
-while ($result = mysql_fetch_assoc($tweet)) {
-	$text[] = $result['tweet'];
-	$status_id[] = $result['status_ID'];
+*/
+
+$tweet = $stmt->get_result();
+while ($result = $tweet->fetch_row()) {
+	while ($result2 = $result->fetch_assoc()) {
+		echo $result2;
+	}
+	$text[] = $result['0'];
+	print_r($result);
+	print_r($text);
+	echo count($text);
+/*	echo is_array($result) ? 'Array' : 'Not an Array';
+	echo count($result);
+	echo count($text);
+*/
+/*	$status_id[] = $result['status_ID'];
 	$text2 = current($text);
 	$status_id2 = current($status_id);
-	$query = mysql_query("INSERT INTO test_import (field1, field2) VALUES ('{$text2}', '{$status_id2}')");
+	$query = $mysqli->query("INSERT INTO test_import (field1, field2) VALUES ('{$text2}', '{$status_id2}')");
 	mysql_free_result($query);
+*/
+$url = "http://www.viralheat.com/api/sentiment/review.xml?text=" . urlEncode($text) . "&api_key=" . $vh_apiKEY;
+echo $url;
+
 }
+
+/*
 print_r($text);
 print_r($status_id);
+*/
 
 
 
 
-
-$url = "http://www.viralheat.com/"
 
 
 
