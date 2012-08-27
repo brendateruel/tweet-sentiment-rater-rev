d6113e4a14ab423450fdd37d27df0319f4958b46
Brenda <brendateruel@aol.com>
2012-08-26 20:25:03 -0500

    testing out Viralheat API; still need to define getSentiment function

---
+++ sentiment-analysis4.php
@@ -0,0 +1,57 @@
+﻿<?php
+
+require("../secret.php");
+require("twitteroauth/twitteroauth-xml.php");
+session_start();
+
+if(!empty($_SESSION['username'])){  
+    $twitteroauth = new TwitterOAuth($tOauth_apiKey, $tOauth_apiSecret, $_SESSION['oauth_token'], $_SESSION['oauth_secret']);  
+	$session_username = $_SESSION['username'];
+}  
+
+$text = "is this working yet?";
+
+$url = "http://www.viralheat.com/api/sentiment/review.xml?text=" . urlEncode($text) . "&api_key=" . $vh_apiKEY;
+echo $url;
+
+  /**
+   * GET wrapper for Viralheat.
+   */
+  function get($url, $parameters = array()) {
+    $response = $this->Viralheat($url, 'GET', $parameters);
+    if ($this->format === 'xml' && $this->decode_xml) {
+      return simplexml_load_string($response);
+    }
+    return $response;
+  }
+
+print_r($response);
+
+$new_friends_table = "friends_" . $session_username;
+$new_temp_timeline = "temp_timeline_" . $session_username; 
+  
+  
+if(!($stmt = $mysqli->prepare("SELECT tweet, status_ID FROM {$new_temp_timeline} WHERE user_handle='adactio'"))) {
+	     echo "Prepare failed: (" . $mysqli->errno . ") " . $mysqli->error;
+	}
+if (!$stmt->execute()) {
+	     echo "Execution failed: (" . $mysqli->errno . ") " . $mysqli->error;
+	}
+	
+
+
+
+/*
+print_r($text);
+print_r($status_id);
+*/
+
+
+
+
+
+
+
+
+  
+?>
